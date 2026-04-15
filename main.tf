7caaec849a9b199902b0bc398514927ce0393414
adnan <126405134+AdnanISB@users.noreply.github.com>
2026-04-15 18:47:10 +0000

    files added

--- main.tf
+++ main.tf
@@ -1,56 +1,60 @@
-provider "aws" { 
-  region = "us-east-1" 
-} 
- 
-resource "aws_dynamodb_table" "orders" { 
-  name         = "orders-table" 
-  billing_mode = "PAY_PER_REQUEST" 
- 
-  hash_key  = "user_id" 
-  range_key = "order_id" 
- 
-  attribute { 
-    name = "user_id" 
-    type = "S" 
-  } 
- 
-  attribute { 
-    name = "order_id" 
-    type = "S" 
-  } 
- 
-  attribute { 
-    name = "order_status" 
-    type = "S" 
-  } 
- 
-  attribute { 
-    name = "ttl" 
-    type = "N" 
-  } 
- 
- 
-  global_secondary_index { 
-    name            = "OrderStatusIndex" 
-    hash_key        = "order_status" 
-    projection_type = "ALL" 
-  } 
- 
-  point_in_time_recovery { 
-    enabled = true 
-  } 
- 
-  ttl { 
-    attribute_name = "ttl" 
-    enabled        = true 
-  } 
- 
-  server_side_encryption { 
-    enabled = true 
-  } 
- 
-  tags = { 
-    Environment = "dev" 
-    Project     = "DynamoDB-Automation" 
-  } 
+
+
+resource "aws_dynamodb_table" "orders" {
+  name         = var.table_name
+  billing_mode = var.billing_mode
+
+  hash_key  = var.hash_key
+  range_key = var.range_key
+
+  attribute {
+    name = var.hash_key
+    type = "S"
+  }
+
+  attribute {
+    name = var.range_key
+    type = "S"
+  }
+
+  attribute {
+    name = "order_status"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "OrderStatusIndex"
+    hash_key        = "order_status"
+    projection_type = "ALL"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  ttl {
+    attribute_name = var.ttl_attribute
+    enabled        = true
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
+
+  tags = var.tags
 }
+
+resource "aws_dynamodb_table_item" "order1" { 
+  table_name = aws_dynamodb_table.orders.name 
+  hash_key   = "user_id" 
+  range_key  = "order_id" 
+ 
+  item = jsonencode({ 
+    user_id  = { "S" = "U-101" } 
+    order_id = { "S" = "O-5001" } 
+    product  = { "S" = "Laptop" } 
+    price    = { "N" = "1200" } 
+    status   = { "S" = "CONFIRMED" } 
+    ttl      = { "N" = "1735689600" }   # optional (Unix timestamp) 
+  }) 
+} 
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "table_name" { 
+type = string 
+default = "orders-table"
+} 
+variable "billing_mode" { 
+type    = string 
+default = "PAY_PER_REQUEST" 
+} 
+variable "hash_key" { 
+type = string 
+default = "user_id"
+} 
+variable "range_key" { 
+type = string 
+default = "order_id"
+} 
+variable "ttl_attribute" { 
+type    = string 
+default = "ttl" 
+} 
+variable "tags" { 
+type = map(string) 
+default = {
+  Environment = "dev" 
+  Project     = "DynamoDB-Automation" 
+}
+}
